583cc0038cac0da657340848b23d5219f7dd5f22
arkans <82564511+arkansys22@users.noreply.github.com>
2022-01-27 16:44:24 +0700

    up

--- application/views/fronts/footer.php
+++ application/views/fronts/footer.php
@@ -1,11 +1,9 @@
 <!--footer area start-->
 <footer class="footer_widgets">
+  <center>
     <div class="footer_top">
         <div class="container">
             <div class="row">
-              <div class="col-lg-1">
-
-              </div>
               <div class="col-lg-4">
                   <div class="widgets_container">
                     <div class="logo">
@@ -15,38 +13,32 @@
                       <div class="footer_contact">
                           <p style="color:white; font-size:20px">IKUTI KAMI</p>
                           <div class="footer_social">
+                            <center>
                              <ul>
                                  <li><a class="facebook" href="<?php echo $identitas->facebook?>"><i class="icon-facebook"></i></a></li>
                                  <li><a class="youtube" href="<?php echo $identitas->youtube?>"><i class="icon-youtube"></i></a></li>
                                  <li><a class="instagram2" href="<?php echo $identitas->instagram?>"><i class="icon-instagram2"></i></a></li>
                              </ul>
+                           </center>
                          </div>
                       </div>
                   </div>
               </div>
-              <div class="col-lg-3">
+              <div class="col-lg-4">
                  <div class="footer_col_container">
                           <p style="color:white; font-size:20px">HUBUNGI KAMI</p>
                           <div class="footer_contact">
                             <p><?php echo $identitas->alamat?></p>
-                            <br>
-                          </div>
-                          <div class="footer_contact_inner">
-                               <div class="contact_icone">
-                                   <img src="<?php echo base_url()?>assets/frontend/theme/img/icon/icon-phone.png" alt="">
-                               </div>
-                               <div class="contact_text">
-                                   <p>Hotline Free 7/24: <br> <strong><a href="tel:+<?php echo $identitas->no_telp?>"><?php echo $identitas->no_telp?></a> </strong></p>
-                               </div>
-                           </div>
-                           <div class="footer_contact">
+                             <p><a href="tel:+<?php echo $identitas->no_telp?>"><?php echo $identitas->no_telp?></a> </p>
                              <p><?php echo $identitas->email?></p>
                              <br>
                            </div>
 
                   </div>
               </div>
+
               <div class="col-lg-4">
+
                        <p style="color:white; font-size:20px">SISTEM PEMBAYARAN</p>
                        <img src="<?php echo base_url()?>assets/frontend/campur/payment.png" />
 
@@ -60,7 +52,7 @@
     </div>
     <div class="footer_bottom ">
         <div class="container">
-          <center>
+
             <div class="row align-items-center">
                 <div class="col-lg-12 col-md-12">
                     <div class="copyright_area">
@@ -73,8 +65,9 @@
                     </div>
                 </div>
             </div>
-          </center>
+
         </div>
     </div>
+    </center>
 </footer>
 <!--footer area end-->
